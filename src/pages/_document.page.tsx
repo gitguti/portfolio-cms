d83886edcbf4dc154ca9c7e68b8ff1a90fcb2b62
gitguti <gitmelgtrrz1@gmail.com>
2024-05-22 19:25:59 -0500

    favicon and open graph

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -4,15 +4,27 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
         <meta name="theme-color" content="#ffffff" />
+        <meta name="description" content="I ❤️ to build = design + develop" />
+        <meta property="og:title" content="Git's blog" />
+        <meta property="og:description" content="I ❤️ to build = design + develop" />
+        <meta property="og:url" content="www.gitmelgutierrez.com" />
+        <meta property="og:image" content="/assets/svg/og.jpg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Your Site Name" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Git's blog" />
+        <meta name="twitter:description" content="I ❤️ to build = design + develop" />
+        <meta name="twitter:image" content="/og.jpg" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
       </Head>
       <body>
